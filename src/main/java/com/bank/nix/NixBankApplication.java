2a77b153f9602b2f3f371ed88b03b987abce6122
eduardo_decarvalho <eduardobcarvalho@outlook.com>
2020-02-16 13:53:57 -0300

    First commit

---
+++ src/main/java/com/bank/nix/NixBankApplication.java
@@ -0,0 +1,13 @@
+package com.bank.nix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NixBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NixBankApplication.class, args);
+	}
+
+}
