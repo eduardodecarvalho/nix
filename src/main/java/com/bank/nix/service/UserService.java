43e82e8a9c06aa54da3b2790d57538375869e9f1
eduardo_decarvalho <eduardobcarvalho@outlook.com>
2020-02-22 13:55:47 -0300

    Add exception and verify if datas is correct

--- src/main/java/com/bank/nix/service/UserService.java
+++ src/main/java/com/bank/nix/service/UserService.java
@@ -1,6 +1,7 @@
 package com.bank.nix.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -22,10 +23,17 @@ public class UserService {
 
 	public User findById(Long id) {
 		return userRepository.findById(id)
-				.orElseThrow(() -> new NixBusinessException("User not found"));
+				.orElseThrow(() -> new NixBusinessException(NixBusinessException.USER_NOT_FOUND));
 	}
 
 	public User create(UserDTO dto) {
+		if(dto.getName() == null || dto.getRegisteredNumber() == null) {
+			throw new NixBusinessException(NixBusinessException.INVALID_DATA);
+		}
+		Optional<User> verifyRegisteredNumber = userRepository.findByRegisteredNumber(dto.getRegisteredNumber());
+		if(verifyRegisteredNumber.isPresent()) {
+			throw new NixBusinessException(NixBusinessException.REGISTERED_NUMBER_ALREDY_IN_USE);
+		}
 		User user = new User();
 		user.setName(dto.getName());
 		user.setRegisteredNumber(dto.getRegisteredNumber());
--- src/main/java/com/bank/nix/service/exception/NixBusinessException.java
+++ src/main/java/com/bank/nix/service/exception/NixBusinessException.java
@@ -4,6 +4,11 @@ public class NixBusinessException  extends RuntimeException {
 
 	private static final long serialVersionUID = 1L;
 
+	public static final String USER_NOT_FOUND = "user_not_found";
+	public static final String REGISTERED_NUMBER_ALREDY_IN_USE = "registered_number_alredy_in_use";
+
+	public static final String INVALID_DATA = "invalid_data";
+
 	public NixBusinessException(String msg) {
 		super(msg);
 	}
