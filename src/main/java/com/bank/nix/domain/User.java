4a6e81897f0ec4c350526f2553953a2498f4ad8e
eduardo_decarvalho <eduardobcarvalho@outlook.com>
2020-02-29 16:19:33 -0300

    Fixing jpa in user

--- src/main/java/com/bank/nix/domain/User.java
+++ src/main/java/com/bank/nix/domain/User.java
@@ -2,9 +2,8 @@ package com.bank.nix.domain;
 
 import java.util.Set;
 
-import javax.persistence.CascadeType;
+import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -15,53 +14,53 @@ import javax.persistence.Table;
 @Table(name = "USER")
 public class User {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    private String name;
-    private String registeredNumber;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	private String registeredNumber;
+	@ElementCollection(targetClass=Integer.class)
+	private Set<BankAccount> idBankAccounts;
 
-    @OneToMany(mappedBy = "user", fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
-    private Set<BankAccount> bankAccounts;
+	public User() {
+	}
 
-    public User() {
-    }
+	public User(String name, String registeredNumber) {
+		this.name = name;
+		this.registeredNumber = registeredNumber;
+	}
 
-    public User(final String name, final String registeredNumber) {
-        this.name = name;
-        this.registeredNumber = registeredNumber;
-    }
+	public Long getId() {
+		return id;
+	}
 
-    public Long getId() {
-        return id;
-    }
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-    public void setId(final Long id) {
-        this.id = id;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public String getName() {
-        return name;
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    public void setName(final String name) {
-        this.name = name;
-    }
+	public String getRegisteredNumber() {
+		return registeredNumber;
+	}
 
-    public String getRegisteredNumber() {
-        return registeredNumber;
-    }
+	public void setRegisteredNumber(String registeredNumber) {
+		this.registeredNumber = registeredNumber.replaceAll("[^0-9]", "");
+	}
 
-    public void setRegisteredNumber(final String registeredNumber) {
-        this.registeredNumber = registeredNumber.replaceAll("[^0-9]", "");
-    }
+	@OneToMany(mappedBy = "BANK_ACCOUNT")
+	public Set<BankAccount> getIdBankAccounts() {
+		return idBankAccounts;
+	}
 
-//    @OneToMany(mappedBy = "id_user", fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
-    public Set<BankAccount> getBankAccounts() {
-        return bankAccounts;
-    }
+	public void setIdBankAccounts(Set<BankAccount> idBankAccounts) {
+		this.idBankAccounts = idBankAccounts;
+	}
 
-    public void setBankAccounts(final Set<BankAccount> bankAccounts) {
-        this.bankAccounts = bankAccounts;
-    }
 }
